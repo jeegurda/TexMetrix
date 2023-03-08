d6dc78da915cd4afea00e792a788d129b0c15973
Shagen A <asriyan.sh@gmail.com>
2023-03-08 23:18:56 +0300

    update layout, bundle css

--- src/dom.ts
+++ src/dom.ts
@@ -1,9 +1,13 @@
-export const canvas = document.querySelector<HTMLCanvasElement>('#canvas')
-export const textarea = document.querySelector<HTMLInputElement>('#textarea')
-export const sizeInput = document.querySelector<HTMLInputElement>('#input-size')
-export const inputX = document.querySelector<HTMLInputElement>('#input-x')
-export const inputY = document.querySelector<HTMLInputElement>('#input-y')
+export const textInput =
+  document.querySelector<HTMLTextAreaElement>('.text-input')
+export const fontSizeInput =
+  document.querySelector<HTMLInputElement>('.font-size-input')
 export const alignInputs =
-  document.querySelectorAll<HTMLInputElement>('.input-align')
+  document.querySelectorAll<HTMLInputElement>('.align-input')
 export const baselineInputs =
-  document.querySelectorAll<HTMLInputElement>('.input-baseline')
+  document.querySelectorAll<HTMLInputElement>('.baseline-input')
+
+export const canvas =
+  document.querySelector<HTMLCanvasElement>('.canvas canvas')
+
+export const dprInput = document.querySelector<HTMLInputElement>('.dpr-input')
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { checkTMInterface } from './support'
 import { Align, Baseline } from './types'
 import * as dom from './dom'
+import './style.css'
 
 const initFs = 60
 const ff = 'sans-serif'
@@ -12,130 +13,119 @@ const h = 400
 const initAlign = 'start'
 const initBaseline = 'alphabetic'
 
-if (dom.canvas) {
-  const ctx = dom.canvas.getContext('2d')
-
-  if (ctx === null) {
-    throw new Error('ctx died')
-  }
-
-  if (!dom.textarea || !dom.sizeInput || !dom.inputX || !dom.inputY) {
-    throw new Error('dom el missing')
-  }
-
-  let fs = initFs
-  let text = dom.textarea.value
-  let refXPerc = initRefX
-  let refYPerc = initRefY
-
-  let baseline: Baseline = initBaseline
-  let align: Align = initAlign
-
-  ctx.canvas.style.width = `${w}px`
-  ctx.canvas.style.height = `${h}px`
-  ctx.canvas.width = w * dpr
-  ctx.canvas.height = h * dpr
-
-  const init = () => {
-    ctx.scale(dpr, dpr)
-  }
-
-  const draw = () => {
-    let refX = (refXPerc / 100) * w
-    let refY = (refYPerc / 100) * h
-
-    ctx.textAlign = align
-    ctx.textBaseline = baseline
-
-    ctx.clearRect(0, 0, w, h)
-    ctx.font = `${fs}px ${ff}`
-
-    const metrics = ctx.measureText(text)
-
-    ctx.fillText(text, refX, refY)
-
-    const blAlignPath = new Path2D()
-
-    // baseline
-    blAlignPath.moveTo(0, refY)
-    blAlignPath.lineTo(w, refY)
-
-    // align
-    blAlignPath.moveTo(refX, 0)
-    blAlignPath.lineTo(refX, h)
-
-    ctx.strokeStyle = 'rgb(200,0,200)'
-    ctx.stroke(blAlignPath)
-
-    const bbPath = new Path2D()
-
-    // h bb
-    bbPath.moveTo(0, refY - metrics.actualBoundingBoxAscent)
-    bbPath.lineTo(w, refY - metrics.actualBoundingBoxAscent)
-    bbPath.moveTo(0, refY + metrics.actualBoundingBoxDescent)
-    bbPath.lineTo(w, refY + metrics.actualBoundingBoxDescent)
-
-    // v bb
-    bbPath.moveTo(refX - metrics.actualBoundingBoxLeft, 0) // this can return negative for left-aligned text
-    bbPath.lineTo(refX - metrics.actualBoundingBoxLeft, h)
-    bbPath.moveTo(refX + metrics.actualBoundingBoxRight, 0)
-    bbPath.lineTo(refX + metrics.actualBoundingBoxRight, h)
-
-    ctx.strokeStyle = 'rgb(0,0,0)'
-    ctx.stroke(bbPath)
-
-    const fPath = new Path2D()
-
-    // font bb
-    fPath.moveTo(0, refY - metrics.fontBoundingBoxAscent)
-    fPath.lineTo(w, refY - metrics.fontBoundingBoxAscent)
-    fPath.moveTo(0, refY + metrics.fontBoundingBoxDescent)
-    fPath.lineTo(w, refY + metrics.fontBoundingBoxDescent)
-
-    ctx.strokeStyle = 'rgb(240,0,0)'
-    ctx.stroke(fPath)
-  }
-
-  dom.textarea.addEventListener<'input'>('input', (ev) => {
-    const value = (ev.target as HTMLInputElement).value
-    text = value
-    draw()
-  })
-
-  dom.sizeInput.addEventListener('input', (ev) => {
-    const value = (ev.target as HTMLInputElement).value
-    fs = parseInt(value) || initFs
-    draw()
-  })
-
-  dom.inputX.addEventListener('input', (ev) => {
-    const value = (ev.target as HTMLInputElement).value
-    refXPerc = parseInt(value) ?? initRefX
-    draw()
-  })
-  dom.inputY.addEventListener('input', (ev) => {
-    const value = (ev.target as HTMLInputElement).value
-    refYPerc = parseInt(value) ?? initRefY
-    draw()
-  })
-
-  Array.from(dom.alignInputs).forEach((input) =>
-    input.addEventListener('change', (ev) => {
-      const value = (ev.target as HTMLInputElement).value as Align
-      align = value
-      draw()
-    }),
-  )
-  Array.from(dom.baselineInputs).forEach((input) =>
-    input.addEventListener('change', (ev) => {
-      const value = (ev.target as HTMLInputElement).value as Baseline
-      baseline = value
-      draw()
-    }),
-  )
-
-  init()
-  draw()
+if (!dom.canvas || !dom.textInput || !dom.fontSizeInput) {
+  throw new Error('dom el missing')
 }
+
+const ctx = dom.canvas.getContext('2d')
+
+if (ctx === null) {
+  throw new Error('ctx died')
+}
+
+let fs = initFs
+let text = 'jee'
+let refXPerc = initRefX
+let refYPerc = initRefY
+
+let baseline: Baseline = initBaseline
+let align: Align = initAlign
+
+ctx.canvas.style.width = `${w}px`
+ctx.canvas.style.height = `${h}px`
+ctx.canvas.width = w * dpr
+ctx.canvas.height = h * dpr
+
+dom.textInput.value = text
+
+const init = () => {
+  ctx.scale(dpr, dpr)
+}
+
+const draw = () => {
+  let refX = (refXPerc / 100) * w
+  let refY = (refYPerc / 100) * h
+
+  ctx.textAlign = align
+  ctx.textBaseline = baseline
+
+  ctx.clearRect(0, 0, w, h)
+  ctx.font = `${fs}px ${ff}`
+
+  const metrics = ctx.measureText(text)
+
+  ctx.fillText(text, refX, refY)
+
+  const blAlignPath = new Path2D()
+
+  // baseline
+  blAlignPath.moveTo(0, refY)
+  blAlignPath.lineTo(w, refY)
+
+  // align
+  blAlignPath.moveTo(refX, 0)
+  blAlignPath.lineTo(refX, h)
+
+  ctx.strokeStyle = 'rgb(200,0,200)'
+  ctx.stroke(blAlignPath)
+
+  const bbPath = new Path2D()
+
+  // h bb
+  bbPath.moveTo(0, refY - metrics.actualBoundingBoxAscent)
+  bbPath.lineTo(w, refY - metrics.actualBoundingBoxAscent)
+  bbPath.moveTo(0, refY + metrics.actualBoundingBoxDescent)
+  bbPath.lineTo(w, refY + metrics.actualBoundingBoxDescent)
+
+  // v bb
+  bbPath.moveTo(refX - metrics.actualBoundingBoxLeft, 0) // this can return negative for left-aligned text
+  bbPath.lineTo(refX - metrics.actualBoundingBoxLeft, h)
+  bbPath.moveTo(refX + metrics.actualBoundingBoxRight, 0)
+  bbPath.lineTo(refX + metrics.actualBoundingBoxRight, h)
+
+  ctx.strokeStyle = 'rgb(0,0,0)'
+  ctx.stroke(bbPath)
+
+  const fPath = new Path2D()
+
+  // font bb
+  fPath.moveTo(0, refY - metrics.fontBoundingBoxAscent)
+  fPath.lineTo(w, refY - metrics.fontBoundingBoxAscent)
+  fPath.moveTo(0, refY + metrics.fontBoundingBoxDescent)
+  fPath.lineTo(w, refY + metrics.fontBoundingBoxDescent)
+
+  ctx.strokeStyle = 'rgb(240,0,0)'
+  ctx.stroke(fPath)
+}
+
+dom.textInput.addEventListener<'input'>('input', (ev) => {
+  const value = (ev.target as HTMLInputElement).value
+  text = value
+  draw()
+})
+
+dom.fontSizeInput.addEventListener('input', (ev) => {
+  const value = (ev.target as HTMLInputElement).value
+  fs = parseInt(value) || initFs
+  draw()
+})
+
+Array.from(dom.alignInputs).forEach((input) =>
+  input.addEventListener('change', (ev) => {
+    const value = (ev.target as HTMLInputElement).value as Align
+    align = value
+    draw()
+  }),
+)
+Array.from(dom.baselineInputs).forEach((input) =>
+  input.addEventListener('change', (ev) => {
+    const value = (ev.target as HTMLInputElement).value as Baseline
+    baseline = value
+    draw()
+  }),
+)
+
+init()
+draw()
 
 checkTMInterface()
